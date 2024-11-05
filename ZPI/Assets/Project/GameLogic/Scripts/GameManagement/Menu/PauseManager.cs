756caf1c375ddce75335c2b1c14c5ec451c9656e
kooozka <jkozanecki@op.pl>
2024-11-05 16:39:19 +0100

    Merged changes into final scene

--- ZPI/Assets/Project/GameLogic/Scripts/GameManagement/Menu/PauseManager.cs
+++ ZPI/Assets/Project/GameLogic/Scripts/GameManagement/Menu/PauseManager.cs
@@ -9,6 +9,7 @@ public class PauseManager : MonoBehaviour
     public void PauseGame()
     {
         GameState.Instance.IsGamePaused = true;
+        Time.timeScale = 0f;
         PausePanel.SetActive(true);
         Cursor.lockState = CursorLockMode.Confined;
     }
@@ -16,6 +17,7 @@ public class PauseManager : MonoBehaviour
     public void ResumeGame()
     {
         GameState.Instance.IsGamePaused = false;
+        Time.timeScale = 1f;
         PausePanel.SetActive(false);
     }
 }
