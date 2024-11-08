17052b4798aad7d9cc1b28d23cdc9e2624f3066e
Adam Mikołajczyk <69213769+Razuer@users.noreply.github.com>
2024-11-08 20:37:22 +0100

    Add GlassyFireBall material with detailed properties and metadata. Update Explosion_Small_FX prefab's ParticleSystem settings.

--- ZPI/Assets/Project/GameLogic/Scripts/Player/Projectile.cs
+++ ZPI/Assets/Project/GameLogic/Scripts/Player/Projectile.cs
@@ -6,12 +6,26 @@ public class Projectile : MonoBehaviour
 {
     private bool _collided;
     public GameObject ImpactVFX;
+    public bool universalBulletVfx = false;
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Player" && !_collided)
+        if (!collision.gameObject.CompareTag("Bullet") && !collision.gameObject.CompareTag("Player") && !_collided)
         {
             _collided = true;
             var impact = Instantiate(ImpactVFX, collision.contacts[0].point, Quaternion.identity) as GameObject;
+
+            if (universalBulletVfx)
+            {
+                foreach (Transform child in transform)
+                {
+                    child.GetComponent<ParticleSystem>().Stop(true, ParticleSystemStopBehavior.StopEmitting);
+                    Destroy(child.gameObject, 5f);
+                    child.transform.parent = null; // Odłączenie dziecka od sfery
+                }
+
+            }
+
             Destroy(impact, 2);
             Destroy(gameObject);
         }
