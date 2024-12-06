389bce18d92ce69e8623a3b485864ded2ce2a106
kooozka <jkozanecki@op.pl>
2024-12-06 18:17:49 +0100

    Upgrading enemies added, nav mesh bug fixed

---
+++ ZPI/Assets/Project/GameLogic/Scripts/Enemies/EnemiesStats.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesStats : MonoBehaviour
+{
+    public static EnemiesStats Instance { get; private set; }
+
+    private Dictionary<string, float> enemiesHealth = new();
+    private Dictionary<string, float> enemiesDamage = new();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        InitializeStats();
+    }
+
+    private void InitializeStats()
+    {
+        enemiesDamage["Zombie"] = 10f;
+        enemiesDamage["Skeleton"] = 12f;
+        enemiesHealth["Zombie"] = 120f;
+        enemiesHealth["Skeleton"] = 80f;
+    }
+
+    public float GetHealthData(string enemy)
+    {
+        if (enemiesHealth.TryGetValue(enemy, out float value))
+        {
+            return value;
+        }
+        Debug.LogWarning($"Health data for {enemy} not found!");
+        return 0;
+    }
+
+    public float GetDamageData(string enemy)
+    {
+        if (enemiesDamage.TryGetValue(enemy, out float value))
+        {
+            return value;
+        }
+        Debug.LogWarning($"Damage data for {enemy} not found!");
+        return 0;
+    }
+
+    public void SetHealthData(string enemy, float value)
+    {
+        if (enemiesHealth.ContainsKey(enemy))
+        {
+            enemiesHealth[enemy] = value;
+        }
+    }
+
+    public void SetDamageData(string enemy, float value)
+    {
+        if (enemiesDamage.ContainsKey(enemy))
+        {
+            enemiesDamage[enemy] = value;
+        }
+    }
+}
